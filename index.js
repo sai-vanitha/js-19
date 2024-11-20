47eb32afd4479033822355dd88ca854777ce43f3
sai vanitha <saivanitha21221@gamil.com>
2024-11-20 10:47:25 +0530

    js-19

---
+++ index.js
@@ -0,0 +1,37 @@
+1.console.log(a);
+var a =10;
+console.log(a);
+a=20;
+console.log(a);
+
+2.console.log(b);
+var b = 5;
+var b = 15;
+console.log(b);
+b = b + 5;
+console.log(b);
+
+3.console.log(b);
+var b = 5;
+var b = 15;
+console.log(b);
+b = b + 5;
+console.log(b);
+
+4.var d;
+console.log(d);
+d = 50;
+console.log(d);
+d = d + 10;
+console.log(d);
+var d = 100;
+
+5.var e = 1;
+console.log(e);
+e = e + 1;
+console.log(e);
+var e = 10;
+console.log(e);
+e = e * 2;
+console.log(e);
+
